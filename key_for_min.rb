1d94bf39c66cc9fa81948de9acbc5349deaa8742
Keith Burgie <krburgie@gmail.com>
2019-04-30 21:45:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,26 +1,17 @@
 def key_for_min_value(name_hash)
-  sorted_hash = []
-  largest = 0
-  smallest = 1 << 64 #apparently the largest fixnum
+  keyArray = []
+  valueArray = [name_hash.first[1]]
   
   name_hash.each do |key, value|
-    if value > 0
-      if value > largest
-          sorted_hash.push(key)
-          largest = value
-      elsif value < smallest
-          sorted_hash.unshift(key)
-          smallest = value
+    if value <= valueArray[0]
+      valueArray.push(value)
+      if value <= valueArray[0]
+        keyArray.unshift(key)
       end
-    elsif value < 0
-      if value > smallest  
-          sorted_hash.push(key)
-          largest = value
-      elsif value < smallest
-          sorted_hash.unshift(key)
-          smallest = value
-      end
+    elsif value > valueArray[0]
+      valueArray.push(value)
+      keyArray.push(key)
     end
   end
-  sorted_hash[0]
+  puts keyArray[0]
 end
