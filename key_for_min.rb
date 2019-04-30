b958ddc1d5e418caf83d76daddb36d038dd7e2e3
Keith Burgie <krburgie@gmail.com>
2019-04-30 21:45:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,26 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
-
+  sorted_hash = []
+  largest = 0
+  smallest = 1 << 64 #apparently the largest fixnum
+  
+  name_hash.each do |key, value|
+    if value > 0
+      if value > largest
+          sorted_hash.push(key)
+          largest = value
+      elsif value < smallest
+          sorted_hash.unshift(key)
+          smallest = value
+      end
+    elsif value < 0
+      if value > smallest  
+          sorted_hash.push(key)
+          largest = value
+      elsif value < smallest
+          sorted_hash.unshift(key)
+          smallest = value
+      end
+    end
+  end
+  sorted_hash[0]
 end
